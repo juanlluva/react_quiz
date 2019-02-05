de58842f6a60ceec7ad71ea87fb7c6aabe2174f4
juanlluva <jlluva5@gmail.com>
2019-02-05 16:42:06 +0100

    Added build

---
+++ build/precache-manifest.3f765122e4b49d85658f863b9a6b58ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7f0e49a3dc26eeff57db42004a34ed7",
+    "url": "./static/media/download.d7f0e49a.gif"
+  },
+  {
+    "revision": "94c2262dee60d4b39e48970f05c06eac",
+    "url": "./static/media/download.94c2262d.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "1b65525a2b45f0798f76",
+    "url": "./static/js/main.1b65525a.chunk.js"
+  },
+  {
+    "revision": "0cab19c6ed35fc579859",
+    "url": "./static/js/1.0cab19c6.chunk.js"
+  },
+  {
+    "revision": "1b65525a2b45f0798f76",
+    "url": "./static/css/main.1b9843af.chunk.css"
+  },
+  {
+    "revision": "da4e49da1fcd30f6cdd06c1e5977a051",
+    "url": "./index.html"
+  }
+];
